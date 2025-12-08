c86fd22545db8b7528f9262ac3f2a141743514a9
Leonid Sliusar <lslusar35@gmail.com>
2025-12-08 22:19:57 +0700

    sync with production

---
+++ .venv/lib/python3.10/site-packages/pymupdf/_build.py
@@ -0,0 +1,8 @@
+mupdf_location = 'https://mupdf.com/downloads/archive/mupdf-1.26.11-source.tar.gz'
+pymupdf_version = '1.26.6'
+pymupdf_version_tuple = (1, 26, 6)
+pymupdf_git_sha = 'a31bac3be04e88c9e8768dba48e1c4032cd2e09a'
+pymupdf_git_diff = ''
+pymupdf_git_branch = 'main'
+swig_version = '4.4.0'
+swig_version_tuple = (4, 4, 0)
